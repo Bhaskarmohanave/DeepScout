a6d4fe852fdf36531fc24086033ada684ce0f87a
Bhaskar Mohan <121608529+Bhaskarmohanave@users.noreply.github.com>
2025-10-23 01:59:43 +0000

    Backend

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+
+const app  = express()
+app.use(cors({
+    origin:"https://automatic-eureka-j69j57jwqjwfp6xj-5173.app.github.dev/"
+}))
+
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
+
+app.listen(3000, () => console.log("Server Running"))
