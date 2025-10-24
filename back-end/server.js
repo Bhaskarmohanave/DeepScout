a08e0f149ef198e2d6585a57fde13b8f8580fa17
Bhaskar Mohan <121608529+Bhaskarmohanave@users.noreply.github.com>
2025-10-24 13:12:42 +0000

    server side

--- back-end/server.js
+++ back-end/server.js
@@ -1,29 +1,47 @@
-const express = require("express")
-const cors = require("cors")
-const users = require("./fakeDB")
-const app  = express()
+const express = require("express");
+const cors = require("cors");
+const users = require("./fakeDB");
+const app = express();
 
-app.use(express.json())
-app.use(cors({
-    origin:"*",
-    methods: ['GET', 'POST'],
-    credentials: true
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://automatic-eureka-j69j57jwqjwfp6xj-5173.app.github.dev",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.post("/login", (req,res) => {
-    console.log(req.body)
-    const { username, password} = req.body;
+app.post("/login", (req, res) => {
+  try {
+    console.log(req.body);
+    const { userName, password } = req.body;
 
-    const user = users.find((u) => u.username === username && u.password === password)
-    
-    if (user){
-        res.json({success:true, message:"hello", username:user.username})
+    const user = users.find(
+      (u) => u.userName === userName && u.password === password
+    );
+
+    if(!req.body){
+        res.status(400).json({success:false, message: "Credentials are required"})
     }
-    
-})
+    if(!userName||!password||!userName.trim()||!password.trim()){
+        res.status(400).json({success: false, message: "Credentials are required"})
+    }
+    if (user) {
+      res.status(200).json({ success: true, user: user.userName });
+    } else {
+      res
+        .status(401)
+        .json({ success: false, message: "Credentials are not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+    console.log(err);
+  }
+});
 
-app.listen(3000, () => console.log("Server Running"))
+app.listen(3000, () => console.log("Server Running"));
